54ffb2ddd450b299fe7f3d1a567859f79246a736
RedBrean <redbrean7425@gmail.com>
2024-02-02 11:55:35 +0300

    лекция

--- lec1/cheto.cpp
+++ lec1/cheto.cpp
@@ -3,6 +3,8 @@
 int main()
 {
     
-    std::cout << "123";
-
+    short  a=5, b=5;
+    auto c = a+b;
+    std::cout << sizeof(c) << std::endl;
+    std::cout << sizeof(static_cast<short>(c)) << "\n";
 }
