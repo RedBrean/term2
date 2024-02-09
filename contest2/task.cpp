8a82b04019b37d6f8e00a54b45b9cc30c8538985
RedBrean <redbrean7425@gmail.com>
2024-02-09 11:40:37 +0300

    lec

---
+++ contest2/task.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int fib(long long x)
+{
+    if(x == 0 | x == 1) {return 1;}
+    return fib(x-1) + fib(x-2);
+}
+
+int main()
+{
+    long long x;
+    cin >> x;
+    cout << fib(x);
+}
---
+++ contest2/taskH.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+int NOD(int x, int y){
+    
+}
+
+int main()
+{
+    int a,b,c,x,y;
+    cin >> a >> b >> c;
+
+
+}
