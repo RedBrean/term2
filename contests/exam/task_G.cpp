622f8e7857893bf20829bfa918ec7f37812cd9c5
RedBrean <redbrean7425@gmail.com>
2024-04-25 20:19:27 +0300

    cheta

--- contests/exam/task_G.cpp
+++ contests/exam/task_G.cpp
@@ -12,10 +12,36 @@ using std::string;
 
 bool pattern_match(char const *pattern, char const *str)
 {
-    string
+    int i = 0;
+    int j = 0;
+    while (true)
+    {   
+        if(pattern[i] == '0'){return(str[j] == '0');}
+        if(str[j] == '0'){return(pattern[i] == '0');}
+        char symb = pattern[i];
+        if(symb == '+'){
+            i++;
+            continue;
+        }
+        bool inf = pattern[i+1] == '+';
+        if(inf){
+            while(str[j] == symb){
+                j++;
+                if(str[j] == '0'){
+                    break;
+                }
+            }
+        }
+        else{
+            if(str[j]==symb){j++;}
+            else{return 0; }
+        }
+        i++;
+    }
+    
 }
 
 int main()
 {
-    cout << pattern_match("a+b+0","aaaabbbbb0")
+    cout << pattern_match("a+b+0","aaaabbbbb0");
 }
