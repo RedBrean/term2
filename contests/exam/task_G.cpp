d9de4f9678c1b11537732e8209b6efdc2300318c
RedBrean <redbrean7425@gmail.com>
2024-04-25 13:32:46 +0300

    exam

---
+++ contests/exam/task_G.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+typedef long long ll;
+
+using std::cout;
+using std::cin;
+using std::vector;
+using std::string;
+
+bool pattern_match(char const *pattern, char const *str)
+{
+    string
+}
+
+int main()
+{
+    cout << pattern_match("a+b+0","aaaabbbbb0")
+}
