31a58ff89466a4b43dbd987b45229b34ea24044a
RedBrean <redbrean7425@gmail.com>
2024-09-07 19:18:36 +0300

    work Please

---
+++ term3/sems/sem20240907/make_example/ld_example/main.cpp
@@ -0,0 +1,8 @@
+float foo(float, float);
+
+int main()
+{
+    float x = 3.14, y = 2.71;
+    float p = foo(x, y);
+    return 0;
+}
---
+++ term3/sems/sem20240907/make_example/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#define X 42
+
+int main()
+{
+    std::cout << "Hello, World " << X << "!" << std::endl;   
+}
